6999a96a0ebe147578c46bb304a6eb69013e273c
userprogramersp <dfm8446@gmail.com>
2025-12-15 20:59:35 +0100

    Update Ninots.java

--- workspace/practica/src/programame/Ninots.java
+++ workspace/practica/src/programame/Ninots.java
@@ -7,7 +7,13 @@ public class Ninots {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		leerEntero("hola ", sc, 0, 100);
+		int numeroNombres = leerEntero("Cuantos nombres vas a colocar? ", sc, 1, 1000);
+		
+		String[] nombres = new String[numeroNombres]; 
+		
+		for(int i = 0; i < nombres.length; i++) {
+			
+		}
 		
 	}
 	
@@ -30,6 +36,27 @@ public class Ninots {
 
 		return numero;
 		
+	}
+	
+	public static String leerCadena(String mensaje, Scanner sc, int min, int max) {
+	
+			System.out.println(mensaje);
+			String cadena = sc.nextLine();
+			if(cadena.length() < min || cadena.length() > max){
+				throw new IllegalArgumentException(String.format("La vadena de texto debe estar entre %d - %d caracteres.",min,max));
+			}
+		
+		
+		return cadena;
+	}
+	
+	public static boolean repetido(String cadena, String[] cadenas, int indiceInicio) {
+		for(int i = indiceInicio; i < cadenas.length; i++){
+			if(cadenas[i].equals(cadenas)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
