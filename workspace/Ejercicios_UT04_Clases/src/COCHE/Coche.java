44339bf1e6d7a235ddf464a9acc799b41122d627
userprogramersp <dfm8446@gmail.com>
2025-12-15 20:01:46 +0100

    j

---
+++ workspace/Ejercicios_UT04_Clases/src/COCHE/Coche.java
@@ -0,0 +1,11 @@
+package COCHE;
+
+public class Coche {
+	private boolean arrancado;
+	private boolean marchaAtras;
+	private int velocidadMaxima;
+	private int VelocidadMaximaAtras;
+	private int velocidad;
+	
+	public Coche(boolean arrancado, boolean marchaAtras, )
+}
