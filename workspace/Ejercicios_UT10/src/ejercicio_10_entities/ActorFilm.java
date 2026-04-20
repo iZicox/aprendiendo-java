5e871e77909f19348be598315b7126993e5b21c5
userprogramersp <dfm8446@gmail.com>
2026-04-20 20:52:41 +0200

    actualizacion

--- workspace/Ejercicios_UT10/src/ejercicio_10_entities/ActorFilm.java
+++ workspace/Ejercicios_UT10/src/ejercicio_10_entities/ActorFilm.java
@@ -1,7 +1,31 @@
 package ejercicio_10_entities;
 
-public class ActorFilm {
+import java.util.Objects;
+
+public class ActorFilm implements Comparable<ActorFilm>{
 	//actor_id, film_id
 	private int actorId;
 	private int filmId;
+	@Override
+	public int hashCode() {
+		return Objects.hash(actorId, filmId);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ActorFilm other = (ActorFilm) obj;
+		return actorId == other.actorId && filmId == other.filmId;
+	}
+	@Override
+	public int compareTo(ActorFilm o) {
+		// TODO Auto-generated method stub
+		return this.compareTo(o);
+	}
+	
+	
 }
