c4a1cfc1149660613a8b5acc4e2a519eb4d9b7df
Francisco <53028086+iZicox@users.noreply.github.com>
2026-04-25 20:36:02 +0200

    clases dao

--- tarea/francisco-rosales/src/main/java/Test.java
+++ tarea/francisco-rosales/src/main/java/Test.java
@@ -1,13 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Scanner;
 
+import dao.ILineaTicket;
+import dao.IProductoDAO;
+import dao.ITicketDAO;
+import dao.impl.LineaTicketDAOOracle;
+import dao.impl.ProductoDAOOracle;
+import dao.impl.TicketDAOOracle;
+import entities.LineaTicket;
+import entities.Producto;
 import entities.Ticket;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ticket t = new Ticket(1, LocalDateTime.now(), false);
-		System.out.println(t.toString());
+		IProductoDAO productoDao = new ProductoDAOOracle();
+		ITicketDAO ticketDao = new TicketDAOOracle();
+		ILineaTicket lineaDao = new LineaTicketDAOOracle();
+		String user = "java";
+		String pass = "123";
+		String link = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+		
+		try(Connection con = DriverManager.getConnection(link, user, pass)){
+			
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
--- tarea/francisco-rosales/src/main/java/dao/ITicketDAO.java
+++ tarea/francisco-rosales/src/main/java/dao/ITicketDAO.java
@@ -9,9 +9,24 @@ import entities.Ticket;
 
 public interface ITicketDAO {
 	// crear, buscar, listar, modificar, borrar
+	//**
+	/**
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ID                                        NOT NULL NUMBER(9)
+ FECHAHORA                                 NOT NULL TIMESTAMP(6)
+ TICKETCERRADO                             NOT NULL VARCHAR2(1)
+
+	 * @param con
+	 * @param fechaHora
+	 * @param ticketCerrado
+	 * @return
+	 * @throws SQLException
+	 */
+	
 	Ticket crear(Connection con, LocalDateTime fechaHora, boolean ticketCerrado) throws SQLException;
 	Ticket buscar(Connection con, long id) throws SQLException;
 	List<Ticket> listar(Connection con) throws SQLException;
-	Ticket modificar(Connection con, long id, LocalDateTime fechaHora, boolean ticketCerrado) throws SQLException;
-	Ticket eliminar(Connection con, long id) throws SQLException;
+	boolean modificar(Connection con, long id, boolean ticketCerrado) throws SQLException;
+	boolean eliminar(Connection con, long id) throws SQLException;
 }
