782c6634eaceac69f2f6043c82dcb4dcdc529679
Francisco <53028086+iZicox@users.noreply.github.com>
2026-04-28 20:53:10 +0200

    Update Main.java

--- ejercicio-hotel/com.hotel/src/main/java/com/hotel/Main.java
+++ ejercicio-hotel/com.hotel/src/main/java/com/hotel/Main.java
@@ -1,13 +1,32 @@
 package com.hotel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
 	private static final String[] MENU_GENERAL = {
 			"Mantenimiento de habitaciones"
 	};
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String user = "hotel_java";
+		String pass = "123";
+		String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+		
+		try (Connection con = DriverManager.getConnection(url, user, pass)){
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+			
+		}
+		
+	}
+	
+	private void menuGenera(Connection con) {
+		
 	}
 	
 	private void imprimirMenu(String titulo, String[] menu) {
