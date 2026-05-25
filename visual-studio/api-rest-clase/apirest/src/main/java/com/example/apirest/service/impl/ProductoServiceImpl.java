413aee71ed92aa85b8987ea123a92848b9593285
userprogramersp <dfm8446@gmail.com>
2026-05-25 16:55:56 +0200

    implementacion del servicio

--- visual-studio/api-rest-clase/apirest/src/main/java/com/example/apirest/service/impl/ProductoServiceImpl.java
+++ visual-studio/api-rest-clase/apirest/src/main/java/com/example/apirest/service/impl/ProductoServiceImpl.java
@@ -1,16 +1,59 @@
 package com.example.apirest.service.impl;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.apirest.dto.ProductoDto;
+import com.example.apirest.model.Producto;
 import com.example.apirest.repository.ProductoRepository;
+import com.example.apirest.service.ProductoService;
 
 @Service
-public class ProductoServiceImpl {
+public class ProductoServiceImpl implements ProductoService {
 
     private final ProductoRepository repository;
 
-    public ProductoServiceImpl(ProductoRepository repository){
+    public ProductoServiceImpl(ProductoRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductoDto> listarTodo() {
+        return repository.findAll().stream()
+                .map(ProductoDto::toDto)
+                .toList();
+
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<ProductoDto> buscarPorId(Long id) {
+        return repository.findById(id).map(ProductoDto::toDto);
+    }
+
+    @Transactional
+    public ProductoDto guardar(ProductoDto productoDTO) {
+        Producto producto = productoDTO.toEntity();
+        return ProductoDto.toDto(repository.save(producto));
+    }
+
+    @Transactional
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public ProductoDto actualizar(Long id, ProductoDto dto) {
+        return repository.findById(id)
+                .map(p -> {
+                    p.setNombre(dto.nombre());
+                    p.setPrecio(dto.precio());
+                    return ProductoDto.toDto(repository.save(p));
+                })
+                .orElseThrow(() -> new RuntimeException("No existe el producto con ID: " + id));
     }
 
 }
